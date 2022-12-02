881c935b9f68f1ce24448d9f8d47cd8d57374cb0
SmellyIrishMan <anon@nothanks>
2022-12-01 23:51:33 -0500

    Add project files.

---
+++ AdventOfCode2022/AdventOfCode2022.cpp
@@ -0,0 +1,40 @@
+#include "pch.h"
+#include "framework.h"
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+namespace AdventOfCode
+{
+	using ElfCalorieLists = std::vector<std::vector<unsigned int>>;
+	using ElfCalorieList = std::vector<unsigned int>;
+
+	int64_t Day1HowManyCaloriesPerElf(ElfCalorieLists calorieLists)
+	{
+		int64_t mostCalories = 0;
+		for (const ElfCalorieList& list : calorieLists)
+		{
+			int64_t listTotal = std::accumulate(list.begin(), list.end(), 0);
+			mostCalories = listTotal > mostCalories ? listTotal : mostCalories;
+		}
+
+		return mostCalories;
+	}
+
+	int64_t Day1HowManyCaloriesAmongTopThreeElfs(ElfCalorieLists calorieLists)
+	{
+		std::vector<unsigned int> allTotals;
+		for (const ElfCalorieList& list : calorieLists)
+		{
+			allTotals.emplace_back(std::accumulate(list.begin(), list.end(), 0));
+		}
+
+		int64_t topThreeTotal = 0;
+		const unsigned int maxElements = static_cast<unsigned int>(std::min(allTotals.size(), static_cast<size_t>(3)));
+
+		std::partial_sort(allTotals.begin(), allTotals.begin() + maxElements, allTotals.end(), std::greater<unsigned int>());
+		topThreeTotal = std::accumulate(allTotals.begin(), allTotals.begin() + maxElements, 0);
+
+		return topThreeTotal;
+	}
+}
---
+++ AdventOfCode2022Tests/AdventOfCode2022Tests.cpp
@@ -0,0 +1,82 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../AdventOfCode2022/AdventOfCode2022.cpp"
+#include <filesystem>
+#include <fstream>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace AdventOfCode2022Tests
+{
+	TEST_CLASS(AdventOfCode2022Tests)
+	{
+	public:		
+		TEST_METHOD(Day1Tests)
+		{
+			int64_t result = AdventOfCode::Day1HowManyCaloriesPerElf({ {1'000, 2'000, 3'000}, {4'000}, {5'000, 6'000}, {7'000, 8'000, 9'000}, {10'000} });
+			Assert::AreEqual(int64_t(24'000), result);
+
+			result = AdventOfCode::Day1HowManyCaloriesPerElf({ {1}, {0}, {0}, {0}, {1} });
+			Assert::AreEqual(int64_t(1), result);
+
+			result = AdventOfCode::Day1HowManyCaloriesPerElf({ {1}, {2}, {3}, {4}, {5} });
+			Assert::AreEqual(int64_t(5), result);
+
+			result = AdventOfCode::Day1HowManyCaloriesPerElf({ {1, 2, 3, 4}, {0, 0, 0, 0, 0, 0, 0, 0}, {3}, {4}, {5, 4, 3, 2, 1, 0} });
+			Assert::AreEqual(int64_t(15), result);
+		}
+
+		TEST_METHOD(Day1Part2Tests)
+		{
+			int64_t result = AdventOfCode::Day1HowManyCaloriesAmongTopThreeElfs({ {1'000, 2'000, 3'000}, {4'000}, {5'000, 6'000}, {7'000, 8'000, 9'000}, {10'000} });
+			Assert::AreEqual(int64_t(45'000), result);
+
+			result = AdventOfCode::Day1HowManyCaloriesAmongTopThreeElfs({ {1'000, 2'000, 3'000} });
+			Assert::AreEqual(int64_t(6'000), result);
+
+			result = AdventOfCode::Day1HowManyCaloriesAmongTopThreeElfs({ {1'000, 2'000, 3'000}, {4'000} });
+			Assert::AreEqual(int64_t(10'000), result);
+
+			result = AdventOfCode::Day1HowManyCaloriesAmongTopThreeElfs({ });
+			Assert::AreEqual(int64_t(0), result);
+		}
+
+		TEST_METHOD(Day1ActualInputTest)
+		{
+			int64_t part1Result = 0;
+			int64_t part2Result = 0;
+
+			std::filesystem::path inputPath{ "Data/Day1Input.txt" };
+			Logger::WriteMessage(inputPath.parent_path().c_str());
+			Logger::WriteMessage(std::filesystem::current_path().c_str());
+
+			std::ifstream fileInput{ inputPath };
+			if (fileInput)
+			{
+				AdventOfCode::ElfCalorieList currentList;
+				AdventOfCode::ElfCalorieLists allElfsLists;
+				std::string line;
+				while(!fileInput.eof())
+				{
+					std::getline(fileInput, line);
+					if (line.empty())
+					{
+						allElfsLists.push_back(std::move(currentList));
+						currentList = AdventOfCode::ElfCalorieList();
+					}
+					else
+					{
+						currentList.push_back(std::stoul(line));
+					}
+				}
+
+				part1Result = AdventOfCode::Day1HowManyCaloriesPerElf(allElfsLists);
+				part2Result = AdventOfCode::Day1HowManyCaloriesAmongTopThreeElfs(allElfsLists);
+			}
+			
+			
+			Assert::AreEqual(int64_t(69'310), part1Result);
+			Assert::AreEqual(int64_t(206'104), part2Result);
+		}
+	};
+}
