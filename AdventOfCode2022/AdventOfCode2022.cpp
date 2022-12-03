3d17ed5d54c1768b5a8281641725c5d45a75f7bc
SmellyIrishMan <anon@nothanks>
2022-12-02 23:47:38 -0500

    Day2

--- AdventOfCode2022/AdventOfCode2022.cpp
+++ AdventOfCode2022/AdventOfCode2022.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <numeric>
 #include <algorithm>
+#include <unordered_map>
 
 namespace AdventOfCode
 {
@@ -34,5 +35,68 @@ namespace AdventOfCode
 		std::partial_sort(allTotals.begin(), allTotals.begin() + maxElements, allTotals.end(), std::greater<unsigned int>());
 
 		return std::accumulate(allTotals.begin(), allTotals.begin() + maxElements, 0);;
+	}
+
+	//Rock = 0
+	//Paper = 1
+	//Scissors = 2
+	using RockPaperScissorsTurn = std::pair<int, int>;
+	using RockPaperScissorsTurnScore = std::pair<int, int>;
+	using RockPaperScissorsTurnOutcomes = std::unordered_map<int, std::vector<int>>;
+	
+	RockPaperScissorsTurnScore RockPaperScissorsScoreRound(RockPaperScissorsTurn turn)
+	{
+		constexpr unsigned int maxScore = 6;
+		static const RockPaperScissorsTurnOutcomes scoreOutcomes = { 
+			{0, {3, 0, 6}}, //Rock vs Rock, Paper, Scissors
+			{1, {6, 3, 0}}, //Paper vs Rock, Paper, Scissors
+			{2, {0, 6, 3}}  //Scissors vs Rock, Paper, Scissors
+		};
+
+		const auto scores = scoreOutcomes.find(turn.first);
+		if (scores != scoreOutcomes.end())
+		{
+			unsigned int firstPlayerScore = scores->second[turn.second];
+			firstPlayerScore += turn.first + 1;
+			unsigned int secondPlayerScore = maxScore - firstPlayerScore + turn.second + 1;
+			secondPlayerScore += turn.first + 1;
+			return { firstPlayerScore, secondPlayerScore };
+		}
+
+		return { 0, 0 };
+	}
+	
+	unsigned int Day2RockPaperScissorsStrategyGuide(std::vector<RockPaperScissorsTurn> Turns)
+	{
+		unsigned int myScore = 0;
+		for (const RockPaperScissorsTurn& turn : Turns)
+		{
+			RockPaperScissorsTurnScore Score = RockPaperScissorsScoreRound(turn);
+			myScore += Score.second;
+		}
+
+		return myScore;
+	}
+
+	unsigned int Day2RockPaperScissorsStrategyGuideUnlocked(std::vector<RockPaperScissorsTurn> Turns)
+	{
+		unsigned int myScore = 0;
+		for (const RockPaperScissorsTurn& turn : Turns)
+		{
+			static const RockPaperScissorsTurnOutcomes forcedOutcomes = {
+				{0, {2, 0, 1}}, //Rock vs Lose, Draw, Win
+				{1, {0, 1, 2}}, //Paper vs Lose, Draw, Win
+				{2, {1, 2, 0}}  //Scissors vs Lose, Draw, Win
+			};
+
+			const auto forcedOutcome = forcedOutcomes.find(turn.first);
+			if (forcedOutcome != forcedOutcomes.end())
+			{
+				RockPaperScissorsTurnScore Score = RockPaperScissorsScoreRound({ turn.first, forcedOutcome->second[turn.second] });
+				myScore += Score.second;
+			}
+		}
+
+		return myScore;
 	}
 }
--- AdventOfCode2022Tests/AdventOfCode2022Tests.cpp
+++ AdventOfCode2022Tests/AdventOfCode2022Tests.cpp
@@ -75,5 +75,43 @@ namespace AdventOfCode2022Tests
 			Assert::AreEqual(int64_t(69'310), part1Result);
 			Assert::AreEqual(int64_t(206'104), part2Result);
 		}
+
+		TEST_METHOD(Day2Tests)
+		{
+			std::vector<AdventOfCode::RockPaperScissorsTurn> Turns = { {0, 1}, {1, 0}, {2, 2} };
+			unsigned int result = AdventOfCode::Day2RockPaperScissorsStrategyGuide(Turns);
+			Assert::AreEqual(15u, result);
+
+			result = AdventOfCode::Day2RockPaperScissorsStrategyGuideUnlocked(Turns);
+			Assert::AreEqual(12u, result);
+		}
+
+		TEST_METHOD(Day2ActualInputTest)
+		{
+			unsigned int part1Result = 0;
+			unsigned int part2Result = 0;
+
+			std::filesystem::path inputPath{ "Data/Day2Input.txt" };
+
+			std::ifstream fileInput{ inputPath };
+			if (fileInput)
+			{
+				std::vector<AdventOfCode::RockPaperScissorsTurn> turns;
+				std::string line;
+				while (!fileInput.eof())
+				{
+					std::getline(fileInput, line);
+					unsigned int player1Move = line[0] - 'A';
+					unsigned int player2Move = line[2] - 'X';
+					turns.emplace_back(player1Move, player2Move);
+				}
+
+				part1Result = AdventOfCode::Day2RockPaperScissorsStrategyGuide(turns);
+				part2Result = AdventOfCode::Day2RockPaperScissorsStrategyGuideUnlocked(turns);
+			}
+
+			Assert::AreEqual(14'297u, part1Result);
+			Assert::AreEqual(10'498u, part2Result);
+		}
 	};
 }
