6c653adc6a675f4e822b1e24b0add117453343af
SmellyIrishMan <anon@nothanks>
2022-12-05 01:09:43 -0500

    Day 3 clean up, better use of algorithms

--- AdventOfCode2022/Day3.cpp
+++ AdventOfCode2022/Day3.cpp
@@ -27,58 +27,38 @@ namespace AdventOfCode
 		unsigned int commonItemPriorityTotal = 0;
 		for (const std::string backpack : backpackList)
 		{
-			std::string firstCompartment = backpack.substr(0, backpack.size() / 2);
-			std::unordered_set<char> itemsInFirstComparment;
-			for (const char c : firstCompartment)
-			{
-				itemsInFirstComparment.insert(c);
-			}
+			std::unordered_set<char> itemsInFirstComparment(begin(backpack), begin(backpack) + (backpack.size() / 2));
+			std::unordered_set<char> itemsInSecondComparment(begin(backpack) + (backpack.size() / 2), end(backpack));
 
-			std::string secondComparment = backpack.substr(backpack.size() / 2);
-			char commonItem = '-';
-			for (const char c : secondComparment)
-			{
-				if (itemsInFirstComparment.contains(c))
-				{
-					commonItem = c;
-					break;
-				}
-			}
-			commonItemPriorityTotal += ConvertCharToPriority(commonItem);
+			auto commonItemItr = std::find_first_of(
+				begin(itemsInFirstComparment), end(itemsInFirstComparment),
+				begin(itemsInSecondComparment), end(itemsInSecondComparment));
+
+			commonItemPriorityTotal += ConvertCharToPriority(*commonItemItr);
 		}
 		return commonItemPriorityTotal;
 	}
 
 	unsigned int Day3TripletBadgeAssignment(BackpackList tripletList)
 	{
-		std::unordered_set<char> matchingItemsInCurrentBackpack;
-		std::for_each(tripletList[0].begin(), tripletList[0].end(), [&](const char& c) {
-			matchingItemsInCurrentBackpack.insert(c);
-		});
-		
-		bool first = true;
-		std::unordered_set<char> itemsInPreviousBackpack;;
-		for (const std::string& backpack : tripletList)
-		{
-			if (first)
-			{
-				first = false;
-			}
-			else
-			{
-				itemsInPreviousBackpack = std::move(matchingItemsInCurrentBackpack);
-				matchingItemsInCurrentBackpack.clear();
+		std::unordered_set<char> firstBackpackItemSet(tripletList[0].begin(), tripletList[0].end());
 
-				for (const char c : backpack)
-				{
-					if (itemsInPreviousBackpack.contains(c))
-					{
-						matchingItemsInCurrentBackpack.insert(c);
-					}
-				}
+		std::unordered_set<char> secondBackpackMatchingItemSet;
+		std::for_each(tripletList[1].begin(), tripletList[1].end(), [&](const char& c) {
+			if (firstBackpackItemSet.contains(c))
+			{
+				secondBackpackMatchingItemSet.insert(c);
 			}
-		}
+			});
 
-		return ConvertCharToPriority(*matchingItemsInCurrentBackpack.begin());
+		std::unordered_set<char> thirdBackpackMatchingItemSet;
+		std::for_each(tripletList[2].begin(), tripletList[2].end(), [&](const char& c) {
+			if (secondBackpackMatchingItemSet.contains(c))
+			{
+				thirdBackpackMatchingItemSet.insert(c);
+			}
+			});
+
+		return ConvertCharToPriority(*thirdBackpackMatchingItemSet.begin());
 	}
 }
